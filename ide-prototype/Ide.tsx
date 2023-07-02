d567de0fd78f630e89a67d0d22a730f94c3e04ff
Michael Belousov <michael.belousov98@gmail.com>
2023-07-02 13:14:01 -0400

    add monaco editor with scheme language

--- ide-prototype/Ide.tsx
+++ ide-prototype/Ide.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TestGraphEditor from "./TestGraphEditor";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api"
 import styles from "./Ide.module.css"
+
+const editorProgramKey = "editorProgram"
+
+export function TextEditor() {
+  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const monacoElem = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (monacoElem.current)
+      setEditor((editor) => {
+        return editor ?? monaco.editor.create(monacoElem.current!, {
+          value: localStorage.getItem(editorProgramKey) ?? "",
+          language: "scheme",
+          theme: "vs-dark",
+        })
+      })
+  }, [monacoElem.current]);
+  return <div className={styles.textEditor} ref={monacoElem} />
+}
 
 export function Ide(_props: Ide.Props) {
   return <div className={styles.split}>
-    <textarea id="editor" />
-    <TestGraphEditor />
+    <TextEditor />
+    <span className={styles.graphEditor}> <TestGraphEditor /> </span>
   </div>;
 }
 
--- ide-prototype/index.tsx
+++ ide-prototype/index.tsx
@@ -2,5 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Ide from "./Ide";
+import './userWorker';
 
 ReactDOM.render(<Ide />, document.querySelector("#react-app"));
