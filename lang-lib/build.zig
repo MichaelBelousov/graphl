c4929e9ebc54e18aa7447c8f95664be09662c9de
Michael Belousov <michael.belousov98@gmail.com>
2023-07-29 10:26:02 -0400

    working on zig 0.11 build.zig upgrade

--- lang-lib/build.zig
+++ lang-lib/build.zig
@@ -1,39 +1,52 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("graph-lang", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const lib = b.addStaticLibrary(.{
+        .name = "graph-lang",
+        .root_source_file = std.build.FileSource.relative("src/main.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+    b.installArtifact(lib);
     lib.force_pic = true;
 
-    const main_tests = b.addTest("src/main.zig");
+    const main_tests = b.addTest(.{
+        .root_source_file = std.build.FileSource.relative("src/main.zig"),
+        .optimize = optimize,
+    });
     // test use file buffer
     main_tests.linkLibC();
-    main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
     const web_step = b.step("web", "Build for web");
-    const web_lib = b.addExecutable("graph-lang", "src/main.zig");
-    web_lib.setBuildMode(mode);
     var web_target = target;
     web_target.cpu_arch = .wasm32;
     web_target.os_tag = .freestanding;
-    web_lib.setTarget(web_target);
-    web_lib.install();
+    const web_lib = b.addExecutable(.{
+        .name = "graph-lang",
+        .root_source_file = std.build.FileSource.relative("src/main.zig"),
+        .target = web_target,
+        .optimize = optimize,
+    });
+    b.installArtifact(web_lib);
+
     web_step.dependOn(&web_lib.install_step.?.step);
 
     const ide_json_gen_step = b.step("ide-json-gen", "Build ide-json-gen");
-    const ide_json_gen = b.addExecutable("ide-json-gen", "src/ide_json_gen.zig");
-    ide_json_gen.setBuildMode(mode);
-    ide_json_gen.setTarget(target);
+    const ide_json_gen = b.addExecutable(.{
+        .name = "ide-json-gen",
+        .root_source_file = "src/ide_json_gen.zig",
+        .optimize = optimize,
+        .target = target,
+    });
     ide_json_gen.linkLibC(); // for mmap on linux
-    ide_json_gen.install();
+    b.installArtifact(ide_json_gen);
     ide_json_gen_step.dependOn(&ide_json_gen.install_step.?.step);
 }
