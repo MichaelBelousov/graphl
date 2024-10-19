9aacdf6e0b968fae77488ce10374ed11a9b248c6
Michael Belousov <michael.belousov98@gmail.com>
2024-10-18 22:29:36 -0400

    demo working now

--- ide-prototype/dvui-frontend/build.zig
+++ ide-prototype/dvui-frontend/build.zig
@@ -49,13 +49,14 @@ pub fn build(b: *std.Build) void {
         .target = b.host,
     });
     const cb_run = b.addRunArtifact(cb);
-    cb_run.addFileArg(dvui_dep.path("src/backends/index.html"));
+    cb_run.addFileArg(b.path("index.html"));
     cb_run.addFileArg(dvui_dep.path("src/backends/WebBackend.js"));
     cb_run.addFileArg(exe.getEmittedBin());
     const output = cb_run.captureStdOut();
 
     b.getInstallStep().dependOn(&b.addInstallFileWithDir(output, .prefix, "bin/index.html").step);
     b.getInstallStep().dependOn(&b.addInstallFileWithDir(dvui_dep.path("src/backends/WebBackend.js"), .prefix, "bin/WebBackend.js").step);
+    b.getInstallStep().dependOn(&b.addInstallFileWithDir(b.path("./vite.config.ts"), .prefix, "bin/vite.config.ts").step);
     b.getInstallStep().dependOn(&install_exe.step);
 
     // This *creates* a Run step in the build graph, to be executed when another
