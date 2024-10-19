36f420af010f894a81dd0abf4e32e58da6c0c9b8
Michael Belousov <michael.belousov98@gmail.com>
2024-10-18 22:02:03 -0400

    working on dvui setup

---
+++ ide-prototype/dvui-frontend/build.zig
@@ -0,0 +1,113 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const native_target = b.standardTargetOptions(.{});
+
+    const web_target = std.Target.Query{
+        .cpu_arch = .wasm32,
+        .os_tag = .freestanding,
+        .abi = .musl,
+    };
+
+    const dvui_dep = b.dependency("dvui", .{});
+
+    // const dvui_mod_web = b.addModule("dvui_web", .{
+    //     .root_source_file = b.path("src/dvui.zig"),
+    //     .target = b.resolveTargetQuery(web_target),
+    //     .optimize = optimize,
+    // });
+    //
+    // dvui_mod_web.addCSourceFiles(.{
+    //     .files = &.{
+    //         "src/stb/stb_image_impl.c",
+    //         "src/stb/stb_truetype_impl.c",
+    //     },
+    //     .flags = &.{"-DINCLUDE_CUSTOM_LIBC_FUNCS=1"},
+    // });
+    //
+    // dvui_mod_web.addIncludePath(b.path("src/stb"));
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "dvui-frontend",
+        .root_source_file = b.path("src/main.zig"),
+        .target = web_target,
+        .optimize = optimize,
+        .link_libc = true,
+        .strip = switch (optimize) {
+            .ReleaseFast, .ReleaseSmall => true,
+            else => false,
+        },
+    });
+
+    exe.entry = .disabled;
+
+    exe.root_module.addImport("dvui", dvui_dep.module("dvui_web"));
+    exe.root_module.addImport("WebBackend", dvui_dep.module("WebBackend"));
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    const install_exe = b.addInstallArtifact(exe, .{
+        .dest_dir = .{ .override = .{ .custom = "bin" } },
+    });
+
+    const cb = b.addExecutable(.{
+        .name = "cacheBuster",
+        .root_source_file = dvui_dep.path("src/cacheBuster.zig"),
+        .target = b.host,
+    });
+    const cb_run = b.addRunArtifact(cb);
+    cb_run.addFileArg(dvui_dep.path("src/backends/index.html"));
+    cb_run.addFileArg(dvui_dep.path("src/backends/WebBackend.js"));
+    cb_run.addFileArg(exe.getEmittedBin());
+    const output = cb_run.captureStdOut();
+
+    b.getInstallStep().dependOn(&b.addInstallFileWithDir(output, .prefix, "bin/index.html").step);
+    b.getInstallStep().dependOn(&b.addInstallFileWithDir(dvui_dep.path("src/backends/WebBackend.js"), .prefix, "bin/WebBackend.js").step);
+    b.getInstallStep().dependOn(install_exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = native_target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
