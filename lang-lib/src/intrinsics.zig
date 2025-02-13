3953fff3e187fbc7617d5f2a6c9d54396cc5c207
Michael Belousov <michael.belousov98@gmail.com>
2025-02-13 11:20:26 -0500

    snapshot of when I had it with comptime for loop

--- lang-lib/src/intrinsics.zig
+++ lang-lib/src/intrinsics.zig
@@ -17,11 +17,11 @@ const alloc = if (builtin.cpu.arch.isWasm())
 else
     std.testing.failing_allocator;
 
-fn __grappl_alloc(len: u32) callconv(.C) ?*anyopaque {
+pub fn __grappl_alloc(len: u32) callconv(.C) ?*anyopaque {
     return (alloc.allocWithOptions(u8, len, @sizeOf(u32), null) catch return null).ptr;
 }
 
-fn __grappl_free(ptr: ?*anyopaque, len: u32) callconv(.C) void {
+pub fn __grappl_free(ptr: ?*anyopaque, len: u32) callconv(.C) void {
     if (ptr == null) return;
     const multi_ptr: [*]u8 = @as([*]u8, @ptrCast(ptr));
     return alloc.free(multi_ptr[0..len]);
@@ -43,7 +43,7 @@ pub const GrapplString = extern struct {
 };
 
 /// -1 if doesn't exist
-fn __grappl_string_indexof(str: *const GrapplString, chr: GrapplChar) callconv(.C) i32 {
+pub fn __grappl_string_indexof(str: *const GrapplString, chr: GrapplChar) callconv(.C) i32 {
     const ptr: [*]u8 = @ptrFromInt(str.ptr);
     for (ptr[0..str.len], 0..) |c, i| {
         // TODO: utf8
@@ -54,11 +54,11 @@ fn __grappl_string_indexof(str: *const GrapplString, chr: GrapplChar) callconv(.
     return -1;
 }
 
-fn __grappl_string_len(str: *const GrapplString) callconv(.C) u32 {
+pub fn __grappl_string_len(str: *const GrapplString) callconv(.C) u32 {
     return str.len;
 }
 
-fn __grappl_string_join(a: *const GrapplString, b: *const GrapplString) callconv(.C) *GrapplString {
+pub fn __grappl_string_join(a: *const GrapplString, b: *const GrapplString) callconv(.C) *GrapplString {
     const data = alloc.alloc(u8, a.len + b.len) catch unreachable;
     @memcpy(data[0..a.len], a);
     @memcpy(data[a.len .. a.len + b.len], b);
@@ -70,7 +70,7 @@ fn __grappl_string_join(a: *const GrapplString, b: *const GrapplString) callconv
     return str;
 }
 
-fn __grappl_string_equal(a: *const GrapplString, b: *const GrapplString) callconv(.C) GrapplBool {
+pub fn __grappl_string_equal(a: *const GrapplString, b: *const GrapplString) callconv(.C) GrapplBool {
     if (a.len != b.len)
         return 0;
 
@@ -87,10 +87,10 @@ fn __grappl_string_equal(a: *const GrapplString, b: *const GrapplString) callcon
 // FIXME: definitely this should be implemented in the language
 // via generics, it doesn't make much sense to do this with
 // fixed types
-fn __grappl_max(a: i32, b: i32) callconv(.C) i32 {
+pub fn __grappl_max(a: i32, b: i32) callconv(.C) i32 {
     return @max(a, b);
 }
-fn __grappl_min(a: i32, b: i32) callconv(.C) i32 {
+pub fn __grappl_min(a: i32, b: i32) callconv(.C) i32 {
     return @min(a, b);
 }
 
@@ -101,18 +101,18 @@ pub const GrapplVec3 = extern struct {
 };
 
 // TODO: force inline this in the compiler
-fn __grappl_vec3_x(v: *const GrapplVec3) callconv(.C) f64 {
+pub fn __grappl_vec3_x(v: *const GrapplVec3) callconv(.C) f64 {
     return v.x;
 }
-fn __grappl_vec3_y(v: *const GrapplVec3) callconv(.C) f64 {
+pub fn __grappl_vec3_y(v: *const GrapplVec3) callconv(.C) f64 {
     return v.y;
 }
-fn __grappl_vec3_z(v: *const GrapplVec3) callconv(.C) f64 {
+pub fn __grappl_vec3_z(v: *const GrapplVec3) callconv(.C) f64 {
     return v.z;
 }
 
 // FIXME: use the stack instead of heap allocating for this
-fn __grappl_make_vec3(x: f64, y: f64, z: f64) callconv(.C) *GrapplVec3 {
+pub fn __grappl_make_vec3(x: f64, y: f64, z: f64) callconv(.C) *GrapplVec3 {
     const result = alloc.create(GrapplVec3) catch unreachable;
     result.* = GrapplVec3{ .x = x, .y = y, .z = z };
     return result;
@@ -121,45 +121,29 @@ fn __grappl_make_vec3(x: f64, y: f64, z: f64) callconv(.C) *GrapplVec3 {
 pub const GrapplRgba = u32;
 
 // TODO: force inline this in the compiler
-fn __grappl_rgba_r(v: GrapplRgba) callconv(.C) u8 {
+pub fn __grappl_rgba_r(v: GrapplRgba) callconv(.C) u8 {
     return @intCast((v >> 24) & 0xff);
 }
-fn __grappl_rgba_g(v: GrapplRgba) callconv(.C) u8 {
+pub fn __grappl_rgba_g(v: GrapplRgba) callconv(.C) u8 {
     return @intCast((v >> 16) & 0xff);
 }
-fn __grappl_rgba_b(v: GrapplRgba) callconv(.C) u8 {
+pub fn __grappl_rgba_b(v: GrapplRgba) callconv(.C) u8 {
     return @intCast((v >> 8) & 0xff);
 }
-fn __grappl_rgba_a(v: GrapplRgba) callconv(.C) u8 {
+pub fn __grappl_rgba_a(v: GrapplRgba) callconv(.C) u8 {
     return @intCast((v >> 0) & 0xff);
 }
-fn __grappl_make_rgba(r: i32, g: i32, b: i32, a: i32) callconv(.C) GrapplRgba {
+pub fn __grappl_make_rgba(r: i32, g: i32, b: i32, a: i32) callconv(.C) GrapplRgba {
     return @intCast(((r & 0xff) << 24) | ((g & 0xff) << 16) | ((b & 0xff) << 8) | ((a & 0xff) << 0));
 }
 
 comptime {
     if (builtin.cpu.arch.isWasm()) {
-        // NOTE: I wish I could do a loop over @This() and export every function that starts with
-        // __grappl_, but if I make them `pub` then they are analyzed somehow by the 64-bit targeting
-        // compiler
-        // so instead I manually do:
-        // grep -Po 'fn __grappl_(\w)+' src/intrinsics.zig | sort -u
-        @export(__grappl_alloc, .{ .name = "__grappl_alloc", .linkage = .strong });
-        @export(__grappl_free, .{ .name = "__grappl_free", .linkage = .strong });
-        @export(__grappl_make_rgba, .{ .name = "__grappl_make_rgba", .linkage = .strong });
-        @export(__grappl_make_vec3, .{ .name = "__grappl_make_vec3", .linkage = .strong });
-        @export(__grappl_max, .{ .name = "__grappl_max", .linkage = .strong });
-        @export(__grappl_min, .{ .name = "__grappl_min", .linkage = .strong });
-        @export(__grappl_rgba_a, .{ .name = "__grappl_rgba_a", .linkage = .strong });
-        @export(__grappl_rgba_b, .{ .name = "__grappl_rgba_b", .linkage = .strong });
-        @export(__grappl_rgba_g, .{ .name = "__grappl_rgba_g", .linkage = .strong });
-        @export(__grappl_rgba_r, .{ .name = "__grappl_rgba_r", .linkage = .strong });
-        @export(__grappl_string_equal, .{ .name = "__grappl_string_equal", .linkage = .strong });
-        @export(__grappl_string_indexof, .{ .name = "__grappl_string_indexof", .linkage = .strong });
-        @export(__grappl_string_join, .{ .name = "__grappl_string_join", .linkage = .strong });
-        @export(__grappl_string_len, .{ .name = "__grappl_string_len", .linkage = .strong });
-        @export(__grappl_vec3_x, .{ .name = "__grappl_vec3_x", .linkage = .strong });
-        @export(__grappl_vec3_y, .{ .name = "__grappl_vec3_y", .linkage = .strong });
-        @export(__grappl_vec3_z, .{ .name = "__grappl_vec3_z", .linkage = .strong });
+        for (std.meta.declarations(@This())) |_decl| {
+            const decl = @field(@This(), _decl.name);
+            if (std.mem.startsWith(u8, _decl.name, "__grappl_")) {
+                @export(decl, .{ .name = _decl.name, .linkage = .strong });
+            }
+        }
     }
 }
