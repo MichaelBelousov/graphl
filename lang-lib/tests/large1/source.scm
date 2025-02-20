d7a8afd0b499fd2c8a2fcc5a899cbcf191eddb81
Michael Belousov <michael.belousov98@gmail.com>
2025-02-20 08:50:40 -0500

    remove old dead code

--- lang-lib/tests/large1/source.scm
+++
@@ -1,5 +0,0 @@
-(import utils (define-ranged))
-(import utils ((as define-ranged dr)))
-
-(define a (define-ranged 1 100 42))
-(+ 11 a)
--- lang-lib/tests/ue1/README.md
+++
@@ -1,2 +0,0 @@
-test code shamefully stolen via
-[image](https://i.pinimg.com/originals/4b/75/80/4b7580742ac20edd4ce1e19fd5d34415.png)
