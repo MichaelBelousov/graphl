9d61c27c6b0cf794b1af87ad2da0e1efc08f2313
Michael Belousov <michael.belousov98@gmail.com>
2025-03-05 20:38:35 -0500

    add more faqs to site

--- website/src/pages/twin-sync-studio.tsx
+++ website/src/pages/twin-sync-studio.tsx
@@ -22,19 +22,24 @@ const Homepage = () => {
         About the demo:
         <ul>
           <li>Free</li>
-          <li>Similar features to the discontinued iTwin Exporter for Datasmith, but shouldn't crash. (If it does, contact me!)</li>
+          <li>Only known to work in Unreal Engine versions 5.0.3 through 5.3.2,
+            <br/>
+            later Unreal Engine versions have a bug in the level sequence importer, I have submitted a bug report.
+          </li>
+          <li>You must have visual studio installed to use the plugin (if you don't need a combined export, you can avoid it).</li>
+          <li>Similar features to the discontinued iTwin Exporter for Datasmith, but crashes much less on large data. (If it does, contact me!)</li>
           <li>Submit your email below to get an access token and download link within 48 hours</li>
           <li>
-            The demo is not yet code-signed (that costs money) so Windows will warn you and you need to bypass that.
-            If you want to verify your download, the md5 hash of the demo is FIXME.
+            The demo is not yet code-signed, so Windows will warn you and you need to bypass that.
+            If you want to verify your download, the md5 hash of the installer is <code><pre>4d7810965a70238ee11b33fb30be2c58</pre></code>.
           </li>
-          <li>There are known bugs! But submit any you find with the help menu</li>
+          <li>There are known bugs! But submit any you find in the Graphl help menu or by email</li>
           <li>
             Your access token will permit you to install the demo on a maximum of one machine.
             Please respond to your first email if you have a reason to install the demo on another
             machine
           </li>
-          <li>Free demo may be deactivated after a couple months, especially if the iTwin fees I pay prove higher than expected</li>
+          <li>Free demo may stop working in June, 2025</li>
           <li>Follow <a href="https://www.linkedin.com/in/michael-belousov-745ab8238/">Mike's LinkedIn</a> for
             updates while we work on adding a roadmap to the website
           </li>
