4c00c6a8d978f392dd4967b556a505b5fc99b398
Michael Belousov <michael.belousov98@gmail.com>
2025-04-16 09:52:46 -0400

    fix test preload

--- lang-lib/lang-sdks/js/test/e2e.test.ts
+++ lang-lib/lang-sdks/js/test/e2e.test.ts
@@ -1,23 +1,12 @@
 import { describe, expect, it } from "bun:test";
 import { compileGraphltSourceAndInstantiateProgram } from "../index.mts";
 
-// NOTE: preloads don't seem to work in bun test, need to figure out path around that
-// describe("e2e", () => {
-//   it("simple string", async () => {
-//     const program = await compileGraphltSourceAndInstantiateProgram(`
-//       (typeof (foo) i32)
-//       (define (foo) (return 2))
-//     `)
-//     expect(program.functions.foo()).toEqual(2);
-//   });
-// });
-
-async function test() {
-  const program = await compileGraphltSourceAndInstantiateProgram(`
-    (typeof (foo) i32)
-    (define (foo) (return 2))
-  `)
-  expect(program.functions.foo()).toEqual(2);
-}
-
-test();
+describe("e2e", () => {
+  it("simple string", async () => {
+    const program = await compileGraphltSourceAndInstantiateProgram(`
+      (typeof (foo) i32)
+      (define (foo) (return 2))
+    `)
+    expect(program.functions.foo()).toEqual(2);
+  });
+});
