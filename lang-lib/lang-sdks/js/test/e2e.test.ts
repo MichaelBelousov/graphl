da976d9cedb11e30283d16bb5da860aeeccc9b8f
Michael Belousov <michael.belousov98@gmail.com>
2025-04-16 10:13:25 -0400

    fix custom packages for zigar

--- lang-lib/lang-sdks/js/test/e2e.test.ts
+++ lang-lib/lang-sdks/js/test/e2e.test.ts
@@ -1,12 +1,14 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it, setDefaultTimeout } from "bun:test";
 import { compileGraphltSourceAndInstantiateProgram } from "../index.mts";
 
-describe("e2e", () => {
+setDefaultTimeout(1_000_000); // might need to compile zig code
+
+describe("js sdk", () => {
   it("simple string", async () => {
     const program = await compileGraphltSourceAndInstantiateProgram(`
       (typeof (foo) i32)
       (define (foo) (return 2))
-    `)
+    `);
     expect(program.functions.foo()).toEqual(2);
   });
 });
