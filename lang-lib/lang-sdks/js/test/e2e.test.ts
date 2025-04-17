e65c569ba6d27b4598d4cc0fd44682c10fb1320a
Michael Belousov <michael.belousov98@gmail.com>
2025-04-16 21:01:32 -0400

    attempt node runner for bun crash

--- lang-lib/lang-sdks/js/test/e2e.test.ts
+++ lang-lib/lang-sdks/js/test/e2e.test.ts
@@ -1,14 +1,22 @@
-import { describe, expect, it, setDefaultTimeout } from "bun:test";
+//import { describe, expect, it, setDefaultTimeout } from "bun:test";
+import { describe, it } from "node:test";
+import assert from "node:assert";
 import { compileGraphltSourceAndInstantiateProgram } from "../index.mts";
 
-setDefaultTimeout(1_000_000); // might need to compile zig code
+//setDefaultTimeout(1_000_000); // might need to compile zig code
 
 describe("js sdk", () => {
   it("syntax error extra paren", async () => {
-    expect(compileGraphltSourceAndInstantiateProgram(`
-      (typeof (foo) i32)
-      (define (foo) (return 2)))
-    `)).rejects.toThrow(Error("Unmatched closer"));
+    let err: any;
+    try {
+      await compileGraphltSourceAndInstantiateProgram(`
+        (typeof (foo) i32)
+        (define (foo) (return 2)))
+      `)
+    } catch (_err) {
+      err = _err;
+    }
+    assert.deepStrictEqual(err, Error("Unmatched closer"));
     // TODO: handle full diagnostic from native library
   });
 
@@ -17,7 +25,7 @@ describe("js sdk", () => {
       (typeof (foo) i32)
       (define (foo) (return (* (+ 3 4) 5)))
     `);
-    expect(program.functions.foo()).toEqual(35);
+    assert.strictEqual(program.functions.foo(), 35);
   });
 
   it("factorial recursive", async () => {
@@ -29,7 +37,7 @@ describe("js sdk", () => {
               (begin (return 1))
               (begin (return (* n (factorial (- n 1))))))))
     `);
-    expect(program.functions.factorial(5)).toEqual(120);
+    assert.strictEqual(program.functions.factorial(5), 120);
   });
 
   it("factorial iterative", async () => {
@@ -48,7 +56,7 @@ describe("js sdk", () => {
               >!if)))
       
     `);
-    expect(program.functions.factorial(10n)).toEqual(3628800n);
+    assert.strictEqual(program.functions.factorial(10n), 3628800n);
   });
 
   it("return string", async () => {
@@ -56,7 +64,23 @@ describe("js sdk", () => {
       (typeof (foo) string)
       (define (foo) (return "simple"))
     `);
-    expect(program.functions.foo()).toEqual("simple");
+    assert.strictEqual(program.functions.foo(), "simple");
+  });
+
+  it("return (i32 i32)", async () => {
+    const program = await compileGraphltSourceAndInstantiateProgram(`
+      (typeof (foo) (i32 i32))
+      (define (foo) (return 5 10))
+    `);
+    assert.deepStrictEqual(program.functions.foo(), [5, 10]);
+  });
+
+  it("return (i32)", async () => {
+    const program = await compileGraphltSourceAndInstantiateProgram(`
+      (typeof (foo) (i32))
+      (define (foo) (return 5))
+    `);
+    assert.deepEqual(program.functions.foo(), 5);
   });
 
   it("return (string, i32)", async () => {
@@ -64,6 +88,6 @@ describe("js sdk", () => {
       (typeof (foo) (i32 string))
       (define (foo) (return 5 "hello"))
     `);
-    expect(program.functions.foo()).toEqual([5, "hello"]);
+    assert.deepStrictEqual(program.functions.foo(), [5, "hello"]);
   });
 });
