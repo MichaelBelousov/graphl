0189c31a2e9c9015d7340f9c3f5e2e5f1f69a8a2
Michael Belousov <michael.belousov98@gmail.com>
2025-07-06 11:32:03 -0400

    remove old unused files

--- ide/demo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Ide from "./Ide";
-import './userWorker';
-
-ReactDOM.render(
-  <Ide />,
-  document.querySelector("#react-app"),
-);
