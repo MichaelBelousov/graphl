b1017d5cb7be0a53f237046f02a7635ea063041b
Michael Belousov <michael.belousov98@gmail.com>
2025-07-07 12:38:28 -0400

    small go scaffolding

--- lang-lib/lang-sdks/go/graphl_compiler.go
+++ lang-lib/lang-sdks/go/graphl_compiler.go
@@ -1,101 +1,69 @@
 package graphl_compiler
 
 import (
-  "context"
-  "errors"
-  "fmt"
-  "log"
-  "strconv"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/MichaelBelousov/graphl/lang-lib/lang-sdks/go/graphl_types"
 
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
 )
 
-type ValU32 struct { val uint32 }
-type ValU64 struct { val uint64 }
-type ValI32 struct { val int32 }
-type ValI64 struct { val int64 }
-type ValF32 struct { val float32 }
-type ValF64 struct { val float64 }
-type ValString struct { val String }
+type ValU32 struct{ val uint32 }
+type ValU64 struct{ val uint64 }
+type ValI32 struct{ val int32 }
+type ValI64 struct{ val int64 }
+type ValF32 struct{ val float32 }
+type ValF64 struct{ val float64 }
+type ValString struct{ val String }
 
 type Val interface {
 	tryU32() (uint32, Error)
-	tryU64() (uint64, Error)
-	tryI32() (int32, Error)
-	tryI64() (int64, Error)
-	tryF32() (float32, Error)
-	tryF64() (float64, Error)
-	tryString() (string, Error)
+	// tryU64() (uint64, Error)
+	// tryI32() (int32, Error)
+	// tryI64() (int64, Error)
+	// tryF32() (float32, Error)
+	// tryF64() (float64, Error)
+	// tryString() (string, Error)
 
 	getU32() uint32
-	getU64() uint64
-	getI32() int32
-	getI64() int64
-	getF32() float3
-	getF64() float64
-	getString() string
+	// getU64() uint64
+	// getI32() int32
+	// getI64() int64
+	// getF32() float3
+	// getF64() float64
+	// getString() string
 }
 
-type TypeKind int32
-
-// FIXME: how to enum in go?
-const (
-	TypeKindPrimitive GraphlType = 0
-	TypeKindStruct               = 1
-)
-
-type TypeInfo struct {
-	name string
-	kind TypeKind
-}
-
-const (
-	TypeVoid TypeInfo
-)
-// Types = {
-    // export const void_: GraphlType = { name: "void", kind: "primitive", size: 0 };
-    // export const i32: GraphlType = { name: "i32", kind: "primitive", size: 4 };
-    // export const u32: GraphlType = { name: "u32", kind: "primitive", size: 4 };
-    // export const i64: GraphlType = { name: "i64", kind: "primitive", size: 8 };
-    // export const u64: GraphlType = { name: "u64", kind: "primitive", size: 8 };
-    // export const bool: GraphlType = { name: "bool", kind: "primitive", size: 4 };
-    // export const f32: GraphlType = { name: "f32", kind: "primitive", size: 4 };
-    // export const f64: GraphlType = { name: "f64", kind: "primitive", size: 8 };
-    // // FIXME: is this really a primtive? bad name
-    // export const string: GraphlType = { name: "string", kind: "primitive", size: 4 };
-    // export const code: GraphlType = { name: "string", kind: "primitive", size: 0 };
-    // export const rgba: GraphlType = { name: "rgba", kind: "primitive", size: 4 };
-    // export const extern: GraphlType = { name: "extern", kind: "primitive", size: 4 };
-
-    // // TODO: parse structs out of graphl meta section
-    // // of graphl output
-    // export const vec3: GraphlType = {
-    //     name: "vec3",
-    //     kind: "struct",
-    //     size: 24,
-    //     fieldNames: ["x", "y", "z"],
-    //     fieldTypes: [f64, f64, f64],
-    //     fieldOffsets: [0, 8, 16],
-    // };
-// }
-
-
-func getU32() {
+func (val *Val) getU32() uint32 {
 	switch v := val.(type) {
-	case GraphlValU32: return v.val
-	default: return errors.New("not a u32")
+	case ValU32:
+		return v.val
+	default:
+		panic("value was not a u32")
+	}
+}
+
+func (val *Val) tryU32() (uin32, Error) {
+	switch v := val.(type) {
+	case ValU32:
+		return v.val
+	default:
+		return Error("value was not a u32")
 	}
 }
 
 type HostFuncCtx struct {
-	args: []GraphlVal
+	args []Val
 }
 
-type HostEnv map[string]func (*void) *void
+type HostEnv map[string]func(*void) *void
 
 type Program struct {
-
 }
 
 func (p *Program) CallFunc() *void {
--- lang-lib/lang-sdks/go/graphl_types/graphl_types.go
+++ lang-lib/lang-sdks/go/graphl_types/graphl_types.go
@@ -9,35 +9,36 @@ const (
 )
 
 type Info struct {
-	name string
-	kind Kind
-	size uint32
-	fieldNames []string
+	name         string
+	kind         Kind
+	size         uint32
+	fieldNames   []string
 	fieldOffsets []uint32
-	fieldTypes []*Info
+	fieldTypes   []*Info
 }
 
 var Void = Info("void", KindPrimitive, 0)
-I32 := Info("i32", KindPrimitive, 4)
-U32 := Info("u32", KindPrimitive, 4)
-I64 := Info("i64", KindPrimitive, 8)
-U64 := Info("u64", KindPrimitive, 8)
-Bool := Info("bool", KindPrimitive, 4)
-F32 := Info("f32", KindPrimitive, 4)
-F64 := Info("f64", Kind"primTive, 8)
+var I32 = Info("i32", KindPrimitive, 4)
+var U32 = Info("u32", KindPrimitive, 4)
+var I64 = Info("i64", KindPrimitive, 8)
+var U64 = Info("u64", KindPrimitive, 8)
+var Bool = Info("bool", KindPrimitive, 4)
+var F32 = Info("f32", KindPrimitive, 4)
+var F64 = Info("f64", KindPrimtive, 8)
+
 // FIXME: is this really a primitive? bad name
-String := Info("string", KindPrimitive, 4)
-Code := Info("string", KindPrimitive, 0)
-Rgba := Info("rgba", KindPrimitive, 4)
-Extern := Info("extern", KindPrimitive, 4)
+var String = Info("string", KindPrimitive, 4)
+var Code = Info("string", KindPrimitive, 0)
+var Rgba = Info("rgba", KindPrimitive, 4)
+var Extern = Info("extern", KindPrimitive, 4)
 
 // TODO: parse structs out of graphl meta section
 // of graphl output
-Vec3 := Info(
+var Vec3 = Info(
 	"vec3",
-  KindStruct,
+	KindStruct,
 	24,
-	["x", "y", "z"],
-	[0, 8, 16],
-	[F64, F64, F64]
+	[3]string{"x", "y", "z"},
+	[3]uint32{0, 8, 16},
+	[3]*Info{&F64, &F64, &F64},
 )
